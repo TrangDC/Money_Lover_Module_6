4198bea6f2e70b000b4fafca651cb7b79e9f39d1
TrangDC <dinhcongtrang99@gmail.com>
2024-02-19 21:50:15 +0700

    create spring boot project. create basic structure

---
+++ src/main/java/com/example/money_lover_backend/enums/demo.java
@@ -0,0 +1,4 @@
+package com.example.money_lover_backend.enums;
+
+public class demo {
+}
---
+++ src/test/java/com/example/money_lover_backend/MoneyLoverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.money_lover_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLoverBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
