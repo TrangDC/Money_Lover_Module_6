cb409ee66041690cb01dceddd14134fa99549d9c
TCTrung <trungtrinh1325@gmail.com>
2024-02-20 16:04:59 +0700

    done basic sigin sigup form template .no handle onclick yet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
 import './App.css';
+import LoginPage from "./components/LoginPage/LoginPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path='/login' element={<LoginPage />}></Route>
+                <Route path='/register' element={<RegisterPage />}></Route>
+            </Routes>
+        </BrowserRouter>
+
+
     </div>
   );
 }
---
+++ src/components/RegisterPage/RegisterForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+    MDBBtn,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBIcon,
+}
+    from 'mdb-react-ui-kit';
+import {Link} from "react-router-dom";
+
+const RegisterForm = () => {
+    return (
+        <>
+            <MDBCard className='shadow-5 text-center' style={{marginTop: '-130px',marginLeft: '365px', backdropFilter: 'blur(30px)',borderRadius: '1rem', maxWidth: '650px'}}>
+                <MDBCardBody className='p-5 text-center'>
+                    <h2 className="fw-bold text-black mb-5 text-center" style={{marginTop: '-30px'}}>Register</h2>
+                    <MDBRow>
+                        <MDBCol col='10' md='6'>
+                            <p className="text-black-50 mb-3">Using social networking accounts
+                            </p>
+                            <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
+                                <MDBIcon className="m-n3"/>
+                                Sign in with Facebook
+                            </MDBBtn>
+
+                            <MDBBtn className="mb-4 w-100" size="lg" color='danger'>
+                                <MDBIcon className="m-n3"/>
+                                Sign in with Gmail
+                            </MDBBtn>
+                            <MDBBtn className="mb-4 w-100" size="lg" color='dark'>
+                                <MDBIcon className="m-n3"/>
+                                Sign in with Apple
+                            </MDBBtn>
+                        </MDBCol>
+                        <MDBCol col='6' md='6'>
+                            <p className="text-black-50 mb-3">Using Money Lover account</p>
+                            <div>
+                                <MDBInput wrapperClass='mb-4 w-100'  label='Email address' id='formControlLg' type='email' size="lg"/>
+                                <MDBInput wrapperClass='mb-4 w-100' label='Password' id='formControlLg' type='password' size="lg"/>
+                                <p className="small mb-3 pb-lg-2"><a className="text-green-50" href="#!">Forgot password?</a></p>
+                            </div>
+                            <MDBBtn className='w-100 mb-4' size='md' color='success'>REGISTER</MDBBtn>
+                            <p style={{color: 'black'}}>Have you an account? <Link to="/login" style={{color: 'green'}}>Login here</Link></p>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBCardBody>
+            </MDBCard>
+        </>
+    );
+};
+
+export default RegisterForm;
