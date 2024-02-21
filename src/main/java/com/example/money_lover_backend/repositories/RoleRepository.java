0a3840c5035370b3b83f4570921a0b7f553716b4
TrangDC <dinhcongtrang99@gmail.com>
2024-02-21 09:04:04 +0700

    done basic login register with security (without JWT or Oauth)

---
+++ src/main/java/com/example/money_lover_backend/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.money_lover_backend.repositories;
+
+import com.example.money_lover_backend.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
