5916885b82fc59b298fc3be0f4aa1745b741b624
TCTrung <trungtrinh1325@gmail.com>
2024-02-21 11:36:52 +0700

    update-form-edit-user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import HeaderPage from "./components/TransactionsPage/HeaderPage";
 import InformationUser from "./components/UserPage/InformationUser";
+import ManagerUserPage from "./components/UserPage/ManagerUserPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<InformationUser />}></Route>
+                <Route path='/user/manager' element={<ManagerUserPage />}></Route>
                 <Route path='/trans' element={<HeaderPage />}></Route>
                 <Route path='/login' element={<LoginPage />}></Route>
                 <Route path='/register' element={<RegisterPage />}></Route>
--- src/components/UserPage/InformationUser.js
+++ src/components/UserPage/InformationUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
@@ -6,25 +6,23 @@ import { BsPersonFill } from "react-icons/bs";
 import { MDBTypography } from 'mdb-react-ui-kit';
 import { IoMdWallet } from "react-icons/io";
 import { FaLayerGroup } from "react-icons/fa6";
-import { IoLinkSharp } from "react-icons/io5";
-import { MdEvent } from "react-icons/md";
-import { FaArrowsSpin } from "react-icons/fa6";
-import { BsClipboardFill } from "react-icons/bs";
-import { GiReceiveMoney } from "react-icons/gi";
-import { TbToolsOff } from "react-icons/tb";
-import { IoIosAirplane } from "react-icons/io";
-import { RiFileExcel2Fill } from "react-icons/ri";
-import { FaCartShopping } from "react-icons/fa6";
-import { TbPigMoney } from "react-icons/tb";
-import { FaRegQuestionCircle } from "react-icons/fa";
-import { IoSettingsSharp } from "react-icons/io5";
-import { IoInformationCircle } from "react-icons/io5";
-
-
+import { LuLogOut } from "react-icons/lu";
+import {Link} from "react-router-dom";
+import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import ManagerUserPage from "./ManagerUserPage";
 
 const InformationUser = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+
     return (
         <div>
+
             <Container>
                 <div>
                         <Image src="https://w.wallhaven.cc/full/m3/wallhaven-m3vp7y.jpg" roundedCircle style={{marginTop: '50px',marginLeft: '530px',width: '70px', height: '70px'}} />
@@ -34,10 +32,13 @@ const InformationUser = () => {
             </Container>
             <ListGroup style={{marginTop: '45px'}}>
                 <ListGroup.Item>
-                    <MDBTypography>
-                        <BsPersonFill className = "mx-2 mb-2" style={{width: '30px' ,height: '30px'}}/>
-                        <MDBTypography tag='strong'>Account Management</MDBTypography>
-                    </MDBTypography>
+                    <Link onClick={handleShow} className="text-dark">
+                        <MDBTypography>
+                            <BsPersonFill className = "mx-2 mb-2" style={{width: '30px' ,height: '30px'}}/>
+                            <MDBTypography tag='strong'>Account Management</MDBTypography>
+                        </MDBTypography>
+                    </Link>
+
                 </ListGroup.Item>
 
                 <ListGroup.Item variant="secondary">  <p></p> </ListGroup.Item>
@@ -53,73 +54,42 @@ const InformationUser = () => {
                 </ListGroup.Item>
 
                 <ListGroup.Item variant="secondary"><p></p></ListGroup.Item>
-
-                <ListGroup.Item className='text-success'>
-                    <IoLinkSharp className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} color='green'/>
-                        Bank link
-                </ListGroup.Item>
-
-                <ListGroup.Item variant="secondary"><p></p></ListGroup.Item>
-
                 <ListGroup.Item>
-                    <MdEvent className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Event
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <FaArrowsSpin className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Periodic Transactions
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <BsClipboardFill className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Bill
+                    <LuLogOut  className = "mx-2 mb-1" style={{width: '25px' ,height: '25px'}}/>
+                    Logout
                 </ListGroup.Item>
 
-                <ListGroup.Item variant="secondary"> <p></p> </ListGroup.Item>
-
-                <ListGroup.Item>
-                    <GiReceiveMoney className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Paybook
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <TbToolsOff  className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Tools
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <IoIosAirplane  className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Travel Mode
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <RiFileExcel2Fill  className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Export Data To Excel
-                </ListGroup.Item>
-
-                <ListGroup.Item variant="secondary"> <p></p> </ListGroup.Item>
-
-                <ListGroup.Item>
-                    <FaCartShopping className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Shop
-                </ListGroup.Item>
-
-                <ListGroup.Item variant="secondary"> <p></p> </ListGroup.Item>
-
-                <ListGroup.Item>
-                    <TbPigMoney className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Discover Money Lover
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <FaRegQuestionCircle  className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Support
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <IoSettingsSharp className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Setting
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <IoInformationCircle className = "mx-2 mb-1 " style={{width: '25px' ,height: '25px'}} />
-                    Introduce
-                </ListGroup.Item>
 
             </ListGroup>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton >
+                    <Modal.Title style={{marginLeft: '165px'}} >Edit Account</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Container>
+                        <div>
+                            <Image src="https://w.wallhaven.cc/full/m3/wallhaven-m3vp7y.jpg" roundedCircle style={{marginLeft: '195px',width: '60px', height: '60px'}} />
+                            <h5 style={{marginLeft: '175px'}}>User Name</h5>
+                            <h7 style={{marginLeft: '150px'}}>username@gmail.com</h7>
+                        </div>
+                    </Container>
+                    <ManagerUserPage/>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
         </div>
     );
