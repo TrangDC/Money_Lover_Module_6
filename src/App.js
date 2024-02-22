e83579c6742bab9f07df9787498878c5cbf1e0de
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 21:35:51 +0700

    add basic route to setup layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 
 import './App.css';
-import LoginPage from "./components/LoginPage/LoginPage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import RegisterPage from "./components/RegisterPage/RegisterPage";
-import InformationUser from "./components/UserPage/InformationUser";
+import Layout from "./new_src/components/shared/Layout";
+import Main from "./components/HomePage/Main/Main";
 import ManagerUserPage from "./components/UserPage/ManagerUserPage";
-import WalletPage from "./components/UserPage/WalletPage";
-import Home from "./components/HomePage/Home";
 
 function App() {
   return (
     <div className="App">
 
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-
-        {/*<BrowserRouter>*/}
-        {/*    <Routes>*/}
-        {/*        <Route path='/user' element={<InformationUser />}></Route>*/}
-        {/*        <Route path='/user/manager' element={<ManagerUserPage />}></Route>*/}
-        {/*        <Route path='/user/wallet' element={<WalletPage />}></Route>*/}
-        {/*        <Route path='/login' element={<LoginPage />}></Route>*/}
-        {/*        <Route path='/register' element={<RegisterPage />}></Route>*/}
-        {/*        <Route path='/' element={<Dashboard />}></Route>*/}
-        {/*    </Routes>*/}
-        {/*</BrowserRouter>*/}
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<Main />}/>
+                    <Route path="/home" element={<ManagerUserPage />}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
 
     </div>
   );
