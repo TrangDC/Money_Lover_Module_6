1ab96c942a924fd8dbd23e7d936315bb70b9d819
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 23:18:28 +0700

    add logout button with react-icon. nomethod yet

--- src/new_src/components/shared/Sidebar.js
+++ src/new_src/components/shared/Sidebar.js
@@ -4,6 +4,7 @@ import {DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS} from "../../lib
 import {Link, useLocation} from "react-router-dom";
 import classNames from "classnames";
 import {text} from "@fortawesome/fontawesome-svg-core";
+import {HiOutlineLogout} from "react-icons/hi";
 
 const linkClasses =
     'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
@@ -24,7 +25,14 @@ const Sidebar = () => {
             <div className='flex flex-col gap-0.5 pt-2 border-t border-neutral-700'>
                 {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
                 <SidebarLink key={item.index} item={item}/>
-            ))}</div>
+                ))}
+                <div className={classNames('cursor-pointer text-red-500',linkClasses)}>
+                    <span className="text-xl">
+                        <HiOutlineLogout/>
+                    </span>
+                    Logout
+                </div>
+            </div>
         </div>
     );
 };
