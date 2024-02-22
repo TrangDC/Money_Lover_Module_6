d48e06488699e78691a2bce7bdbb0b52efe65023
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 22:15:20 +0700

    create Sidebar component and add to Layout component

--- src/new_src/components/shared/Layout.js
+++ src/new_src/components/shared/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
+import SideBar from "../../../layout/SideBar/SideBar";
 const Layout = () => {
     return (
         <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
-            <div className='bg-sky-100'>sidebar</div>
+            <SideBar/>
             <div className='p-4'>
                 <div className='bg-teal-100'>header</div>
                 <Outlet/>
