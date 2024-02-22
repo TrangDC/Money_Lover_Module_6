dc789180587fe47f50776c9e8f65cac0c7b6204a
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 03:19:04 +0700

    edit className of BuyerProfileChart

--- src/new_src/BuyerProfileChart.js
+++ src/new_src/BuyerProfileChart.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const BuyerProfileChart = () => {
     return (
-        <div>
-            
+        <div className="w-[20rem] h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
+            <strong className="text-gray-700 font-medium">Buyer Profile</strong>
+            <div className="mt-3 w-full flex-1 text-xs">
+
+            </div>
         </div>
     );
 };
