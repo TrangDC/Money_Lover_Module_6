a4dd3f4cf18f40c3fa227189f6e8d3177cf36770
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 21:46:08 +0700

    add className to Layout with white page, set h w to full screen

--- src/new_src/components/shared/Layout.js
+++ src/new_src/components/shared/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Outlet} from 'react-router-dom';
 const Layout = () => {
     return (
-        <div>
+        <div className='flex flex-row bg-neutral-100 h-screen w-screen'>
             <div className='bg-sky-100'>sidebar</div>
             <div className='bg-teal-100'>header</div>
             <Outlet/>
