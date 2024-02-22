14955128fb087e5de42ff5fcad1b266c07fa97ad
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 22:51:18 +0700

    create linkClasses to stylish SidebarLink

--- src/new_src/components/shared/Sidebar.js
+++ src/new_src/components/shared/Sidebar.js
@@ -3,6 +3,10 @@ import {FcBullish} from "react-icons/fc";
 import {DASHBOARD_SIDEBAR_LINKS} from "../../lib/consts";
 import {Link} from "react-router-dom";
 
+const linkClasses =
+    'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
+
+
 const Sidebar = () => {
     return (
         <div className="flex flex-col bg-neutral-900 w-60 p-3">
@@ -24,10 +28,8 @@ export default Sidebar;
 
 function SidebarLink({item}) {
     return (
-        <Link to={item.path}>
-            <span className="text-xl">
-                {item.icon}
-            </span>
+        <Link to={item.path} className={linkClasses}>
+            <span className="text-xl">{item.icon}</span>
             {item.label}
         </Link>
     )
