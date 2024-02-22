54294a63b0d6b87b36c9ff5d879c3559b6d42f11
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 23:33:12 +0700

    create Header component and add to Layout with basic className

---
+++ src/new_src/components/shared/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-white h-16 px-4'>
+            Header
+        </div>
+    );
+};
+
+export default Header;
