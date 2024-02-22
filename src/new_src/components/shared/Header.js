910ebd9c7fe029491081c456aae8f3f58ba14e7d
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 23:42:22 +0700

    setup search input with className

--- src/new_src/components/shared/Header.js
+++ src/new_src/components/shared/Header.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='bg-white h-16 px-4 flex justify-between items-center'>
-            <div>search field</div>
+            <div>
+                <input type='text'
+                       placeholder='Search your wallet...'
+                       className='text-sm focus:outline-none active:outline-none
+                                  h-10 w-[24rem] border border-gray-400 rounded-sm px-4'
+                />
+            </div>
             <div>side button</div>
         </div>
     );
