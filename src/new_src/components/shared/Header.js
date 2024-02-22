99b8d3d7352e97e4f72e5fcf6f7af13a82df351f
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 23:37:13 +0700

    create div search field and side button. set up with className to set one to the left, one to the right

--- src/new_src/components/shared/Header.js
+++ src/new_src/components/shared/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='bg-white h-16 px-4'>
-            Header
+        <div className='bg-white h-16 px-4 flex justify-between items-center'>
+            <div>search field</div>
+            <div>side button</div>
         </div>
     );
 };
