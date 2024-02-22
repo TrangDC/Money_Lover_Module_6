03fd5a33db6409f1b616acfac1e8c0b396d74735
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 22:26:40 +0700

    add icon and name of web. setup classname with gap space, color, size

--- src/new_src/components/shared/Sidebar.js
+++ src/new_src/components/shared/Sidebar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {FcBullish} from "react-icons/fc";
 
 const Sidebar = () => {
     return (
-        <div className="flex flex-col bg-green-600 w-60 p-3">
+        <div className="flex flex-col bg-neutral-900 w-60 p-3">
+            <div className='flex items-center gap-2 px-1 py-3'>
+                <FcBullish fontSize={24}/>
+                <span className='text-neutral-100 text-lg'>Money Lover</span>
+            </div>
             <div className='flex-1'>
                 top part
             </div>
