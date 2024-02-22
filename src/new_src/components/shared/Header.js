bf75706c7824e847a743ad7166b7cf9b1accc865
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 02:24:43 +0700

    done Header component

--- src/new_src/components/shared/Header.js
+++ src/new_src/components/shared/Header.js
@@ -121,10 +121,9 @@ const Header = () => {
                                 leaveFrom="transform opacity-100 scale-100"
                                 leaveTo="transform opacity-0 scale-95"
                             >
-                                <Menu.Items className="origin-top-right z-10 absolute
-                                                       right-0 mt-2 w-48 rounded-sm
-                                                       shadow-md p-1 bg-white ring-1 ring-opacity-5
-                                                       focus:outline-none">
+                                <Menu.Items className="origin-top-right z-10 absolute right-0
+                                                        mt-2 w-48 rounded-sm shadow-md p-1 bg-white
+                                                        ring-1 ring-black ring-opacity-5 focus:outline-none">
                                         <Menu.Item>
                                             {({ active }) => (
                                                 <div
@@ -138,6 +137,40 @@ const Header = () => {
                                                     navigate("/profile")
                                                 )}>
                                                     Your Profile
+                                                </div>
+
+                                            )}
+                                        </Menu.Item>
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <div
+                                                    className={classNames(
+                                                        active && 'bg-gray-100',
+                                                        'active:bg-gray-200 rounded-sm ' +
+                                                        'px-4 py-2 text-gray-700 cursor-pointer ' +
+                                                        'focus:bg-gray-200'
+                                                    )}
+                                                    onClick={() => (
+                                                        navigate("/settings")
+                                                    )}>
+                                                    Settings
+                                                </div>
+
+                                            )}
+                                        </Menu.Item>
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <div
+                                                    className={classNames(
+                                                        active && 'bg-gray-100',
+                                                        'active:bg-gray-200 rounded-sm ' +
+                                                        'px-4 py-2 text-gray-700 cursor-pointer ' +
+                                                        'focus:bg-gray-200'
+                                                    )}
+                                                    onClick={() => (
+                                                        navigate("/logout")
+                                                    )}>
+                                                    Logout
                                                 </div>
 
                                             )}
