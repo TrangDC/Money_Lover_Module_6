3d06b122db4493ff8150e6d261ee835e67a37c93
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 01:49:15 +0700

    add Menu.Item, edit className

--- src/new_src/components/shared/Header.js
+++ src/new_src/components/shared/Header.js
@@ -107,6 +107,45 @@ const Header = () => {
                                                 Hugh Jackson
                                             </span>
                             </div>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                            >
+                                <Menu.Items className="origin-top-right z-10 absolute
+                                                       right-0 mt-2 w-48 rounded-sm
+                                                       shadow-md p-1 bg-white ring-1 ring-opacity-5
+                                                       focus:outline-none">
+                                    <div className="px-1 py-1 ">
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <button
+                                                    className={`${
+                                                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                >
+                                                    {active ? (
+                                                        <EditActiveIcon
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        <EditInactiveIcon
+                                                            className="mr-2 h-5 w-5"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )}
+                                                    Edit
+                                                </button>
+                                            )}
+                                        </Menu.Item>
+                                    </div>
+                                </Menu.Items>
+                            </Transition>
                         </Menu.Button>
                     </div>
                 </Menu>
