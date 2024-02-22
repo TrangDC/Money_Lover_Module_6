fe1d2a8e569aa637ce27cb469d2f291b708f92b2
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 02:41:33 +0700

    change 4 div to 4 BoxWrapper - child function component

--- src/new_src/DashboardStatsGrid.js
+++ src/new_src/DashboardStatsGrid.js
@@ -3,24 +3,29 @@ import React from 'react';
 const DashboardStatsGrid = () => {
     return (
         <div className='flex gap-4 w-full'>
-            <div className="bg-white rounded-sm p-4 flex-1 border
-                            border-gray-200 flex items-center">
+            <BoxWrapper>
 
-            </div>
-            <div className="bg-white rounded-sm p-4 flex-1 border
-                            border-gray-200 flex items-center">
+            </BoxWrapper>
+            <BoxWrapper>
 
-            </div>
-            <div className="bg-white rounded-sm p-4 flex-1 border
-                            border-gray-200 flex items-center">
+            </BoxWrapper>
+            <BoxWrapper>
 
-            </div>
-            <div className="bg-white rounded-sm p-4 flex-1 border
-                            border-gray-200 flex items-center">
+            </BoxWrapper>
+            <BoxWrapper>
 
-            </div>
+            </BoxWrapper>
         </div>
     );
 };
 
 export default DashboardStatsGrid;
+
+function BoxWrapper({children}) {
+    return (
+        <div className='bg-white rounded-sm p-4 flex-1
+                        border border-gray-200 flex items-center'>
+            {children}
+        </div>
+    )
+}
