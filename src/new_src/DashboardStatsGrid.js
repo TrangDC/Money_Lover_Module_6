82ab9fdfd6bf6b8b72ff08626d8429a2f3992d71
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 02:51:53 +0700

    edit BoxWrapper with money and change

--- src/new_src/DashboardStatsGrid.js
+++ src/new_src/DashboardStatsGrid.js
@@ -8,6 +8,13 @@ const DashboardStatsGrid = () => {
                 <div className='rounded-full h-12 w-12 flex items-center justify-center bg-sky-500'>
                     <IoBagHandle className='text-2xl text-white'/>
                 </div>
+                <div>
+                    <span className='text-sm text-gray-500 font-light'>Total Sales</span>
+                    <div className= 'flex items-center'>
+                        <strong className='text-xl text-gray-700 font-semibold'>300000.0 VND</strong>
+                        <span className='text-sm text-green-500 pl-2'>+5000</span>
+                    </div>
+                </div>
             </BoxWrapper>
             <BoxWrapper>
 
