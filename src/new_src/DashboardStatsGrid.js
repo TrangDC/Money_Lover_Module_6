8c47b2eddbb5d31aa0651f705787b9f2b7fd1826
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 02:37:12 +0700

    edit className of DashBoardStatsGrid. show 4 grid with total width full

--- src/new_src/DashboardStatsGrid.js
+++ src/new_src/DashboardStatsGrid.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 const DashboardStatsGrid = () => {
     return (
-        <div className='flex gap-4'>
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
-            <div className="bg-white"></div>
+        <div className='flex gap-4 w-full'>
+            <div className="bg-white rounded-sm p-4 flex-1 border
+                            border-gray-200 flex items-center">
+
+            </div>
+            <div className="bg-white rounded-sm p-4 flex-1 border
+                            border-gray-200 flex items-center">
+
+            </div>
+            <div className="bg-white rounded-sm p-4 flex-1 border
+                            border-gray-200 flex items-center">
+
+            </div>
+            <div className="bg-white rounded-sm p-4 flex-1 border
+                            border-gray-200 flex items-center">
+
+            </div>
         </div>
     );
 };
