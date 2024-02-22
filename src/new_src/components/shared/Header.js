ab5d27bc4f19db4200449a65e08d29a02f58dced
TrangDC <dinhcongtrang99@gmail.com>
2024-02-22 23:49:02 +0700

    add icon search and setup space, border, padding

--- src/new_src/components/shared/Header.js
+++ src/new_src/components/shared/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import {HiOutlineSearch} from "react-icons/hi";
 
 const Header = () => {
     return (
-        <div className='bg-white h-16 px-4 flex justify-between items-center'>
-            <div>
+        <div className='bg-white h-16 px-4 flex justify-between items-center
+                        border-b border-gray-200'>
+            <div className='relative'>
+                <HiOutlineSearch fontSize={20}
+                                 className='text-gray-400 absolute
+                                            top-1/2 -translate-y-1/2 left-3'/>
                 <input type='text'
                        placeholder='Search your wallet...'
                        className='text-sm focus:outline-none active:outline-none
-                                  h-10 w-[24rem] border border-gray-400 rounded-sm px-4'
+                                  h-10 w-[24rem] border border-gray-400 rounded-sm pr-4 pl-11'
                 />
             </div>
             <div>side button</div>
