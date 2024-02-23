e2616b975906d5b6a2751ada105731f458b3c033
vansonai <design97.ae@gmail.com>
2024-02-23 15:12:27 +0700

    fix method update user on controller

--- src/main/java/com/example/money_lover_backend/controllers/UserController.java
+++ src/main/java/com/example/money_lover_backend/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.example.money_lover_backend.controllers;
 
+import com.example.money_lover_backend.enums.ERole;
+import com.example.money_lover_backend.models.Role;
 import com.example.money_lover_backend.models.User;
+import com.example.money_lover_backend.repositories.RoleRepository;
 import com.example.money_lover_backend.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,11 +12,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/api/users")
+@RequestMapping("/api/users/")
 public class UserController {
 
     @Autowired
@@ -22,8 +28,29 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @GetMapping
+    public ResponseEntity<Iterable<User>> findAllCustomer() {
+        List<User> users = (List<User>) userService.findAll();
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findCustomerById(@PathVariable Long id) {
+        Optional<User> userOptional = userService.findById(id);
+        if (!userOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+    }
+
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('USER')")
+//    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         Optional<User> userOptional = userService.findById(id);
         if (!userOptional.isPresent()) {
@@ -31,6 +58,12 @@ public class UserController {
         }
         user.setId(userOptional.get().getId());
         user.setPassword(encoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        Set<Role> rolesSet = new HashSet<Role>();
+        rolesSet.add(userRole);
+        user.setRoles(rolesSet);
         return new ResponseEntity<>(userService.save(user), HttpStatus.OK);
     }
 
