bd2e56074cafb6936a44755225e855ca3561a7f9
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 12:40:17 +0700

    create table in PopularProducts and map data. fix table on RecentOrder

--- src/new_src/RecentOrders.js
+++ src/new_src/RecentOrders.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {getOrderStatus} from "./lib/utils/get-order-status";
+import {format} from "date-fns";
 
 const recentOrderData = [
     {
@@ -68,13 +69,10 @@ const recentOrderData = [
 
 const RecentOrders = () => {
     return (
-        <div>
-            <div className='bg-white px-4 pt-3
-                            pb-4 rounded-sm border
-                            border-gray-200 flex-1'
-            >
-                <strong className="text-gray-700 font-medium">Recent Orders</strong>
-                <table>
+        <div className="bg-white px-4 pt-3 pb-4 rounded-sm border border-gray-200 flex-1">
+            <strong className="text-gray-700 font-medium">Recent Orders</strong>
+            <div className="border-x border-gray-200 rounded-sm mt-3">
+                <table className="w-full text-gray-700">
                     <thead>
                     <tr>
                         <th>ID</th>
@@ -86,24 +84,26 @@ const RecentOrders = () => {
                         <th>Order Status</th>
                     </tr>
                     </thead>
+                    <tbody>
+                    {recentOrderData.map((order) => (
+                        <tr key={order.id}>
+                            <td>
+                                <Link to={`/order/${order.id}`}>#{order.id}</Link>
+                            </td>
+                            <td>
+                                <Link to={`/product/${order.product_id}`}>#{order.product_id}</Link>
+                            </td>
+                            <td>
+                                <Link to={`/customer/${order.customer_id}`}>{order.customer_name}</Link>
+                            </td>
+                            <td>{format(new Date(order.order_date), 'dd MMM yyyy')}</td>
+                            <td>{order.order_total}</td>
+                            <td>{order.shipment_address}</td>
+                            <td>{getOrderStatus(order.current_order_status)}</td>
+                        </tr>
+                    ))}
+                    </tbody>
                 </table>
-                {recentOrderData.map((order) => (
-                    <tr key={order.id}>
-                        <td>
-                            <Link to={`/order/${order.id}`}>#{order.id}</Link>
-                        </td>
-                        <td>
-                            <Link to={`/product/${order.product_id}`}>#{order.product_id}</Link>
-                        </td>
-                        <td>
-                            <Link to={`/customer/${order.customer_id}`}>{order.customer_name}</Link>
-                        </td>
-                        <td>{order.order_date}</td>
-                        <td>{order.order_total}</td>
-                        <td>{order.shipment_address}</td>
-                        <td>{getOrderStatus(order.current_order_status)}</td>
-                    </tr>
-                ))}
             </div>
         </div>
     );
