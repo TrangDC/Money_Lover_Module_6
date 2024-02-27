536ced7ecccaaf3317e586598b061f9fa7a960d3
TrangDC <dinhcongtrang99@gmail.com>
2024-02-27 10:59:21 +0700

    delete unused code from email

--- src/main/java/com/example/money_lover_backend/services/impl/EmailService.java
+++ src/main/java/com/example/money_lover_backend/services/impl/EmailService.java
@@ -18,20 +18,4 @@ public class EmailService {
         mailMessage.setText(content);
         javaMailSender.send(mailMessage);
     }
-    public void sendEmailLockUser(String toEmail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("trangdinhcongtmqt@gmail.com");
-        mailMessage.setTo(toEmail);
-        mailMessage.setSubject("Thông báo khóa tài khoản");
-        mailMessage.setText("Tài khoản của bạn đã bị khóa. Vui lòng liên hệ quản trị viên để mở khóa tài khoản");
-        javaMailSender.send(mailMessage);
-    }
-    public void sendEmailApproved(String toEmail) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("trangdinhcongtmqt@gmail.com");
-        mailMessage.setTo(toEmail);
-        mailMessage.setSubject("Thông báo xác nhận làm giáo viên");
-        mailMessage.setText("Tài khoản của bạn đã được duyệt làm giáo viên. Chúc mừng bạn!");
-        javaMailSender.send(mailMessage);
-    }
 }
