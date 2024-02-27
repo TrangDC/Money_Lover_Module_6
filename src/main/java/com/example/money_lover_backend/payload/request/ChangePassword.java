e0bcfd8d4533e2282fb1eb7013905b46c0c6c860
vansonai <design97.ae@gmail.com>
2024-02-27 23:45:30 +0700

    check error email in use

--- src/main/java/com/example/money_lover_backend/payload/request/ChangePassword.java
+++ src/main/java/com/example/money_lover_backend/payload/request/ChangePassword.java
@@ -10,5 +10,5 @@ import lombok.*;
 public class ChangePassword {
     private String oldPassword;
     private String newPassword;
-    private String comfirmPassword;
+    private String confirmPassword;
 }
